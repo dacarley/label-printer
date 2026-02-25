dc6df57879dad952f20943d395a8c28d3d93227d
David Carley <dacarley@gmail.com>
2026-02-25 11:26:30 -0800

    chore: GH actions workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 
 // Build a single, fully inlined dist/index.html (JS+CSS inlined)
 export default defineConfig({
+  base: "/label-printer/",
   plugins: [viteSingleFile()],
   server: {
     host: true,      // same as "0.0.0.0" (listen on all interfaces)
